89a9f53fb490ec3d9ac48b7049bc43e913086d35
Sabina Jabrayil <sabina.j@code.edu.az>
2016-08-10 16:01:00 +0400

    bawlanqic

---
+++ main/jquery.js
@@ -0,0 +1,4 @@
+// $( ".salam" ).hover(function() {
+//   $( ".panel" ).css({ "opacity": "1","filter":" alpha(opacity=100)","-webkit-transform":" rotateY(0deg) scale(1,1)","-moz-transform":" rotateY(0deg) scale(1,1);"} ); {
+//   };
+// });
